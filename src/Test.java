c76709af9aed32e79cca50d76d665924d2f77aeb
Tillmann <tillmann-rohlfing@protonmail.com>
2023-08-23 21:37:59 +0200

    CLI

---
+++ src/Test.java
@@ -0,0 +1,6 @@
+ public class Test {
+    public static void main(String[] args){
+        String eanCode = "4199049509905";
+        System.out.println("Der EAN-Code " + eanCode + " ist " + EANCode.istKorrekt(eanCode));
+    }
+}
